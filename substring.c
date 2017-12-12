380e5727bc35ed1e5877e773f6ae1e851af7d0cc
josemartin41 <josemartin41@hotmail.com>
2017-12-12 15:16:42 +0100

    Update substring.c

--- substring.c
+++ substring.c
@@ -3,23 +3,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-char *substring(char *str, int startIndex, int endIndex);
-
-void	*errorf(char *str)
-{
-  int i = 0;
-  while (str[i])
-    i++;
-  write(1, str, i);
-  return (NULL);
-}
-
 char	*substring(char *str, int startIndex, int endIndex)
 {
   if (str == NULL)
-    return (errorf("String is Null"));
+    return (NULL);
   if (startIndex > endIndex)
-    return (errorf("StartIndex is greater than EndIndex"));
+    return (NULL);
   int diff = endIndex - startIndex;
   int i = 0;
   char *final;
@@ -32,12 +21,4 @@ char	*substring(char *str, int startIndex, int endIndex)
     }
   final[i] = '\0';
   return (final);
-}
-
-int	main()
-{
-  char str[] = "Jose Martin Garcia Diaz del Valle";
-  char *cuttedStr;
-  cuttedStr = substring(str, 5, 17);
-  printf("%s\n", cuttedStr);
 }
